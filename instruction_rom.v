b55ab969e681582690610b2c2dfe121eaed5e376
willriley <wriley.email@gmail.com>
2020-03-13 22:58:19 -0400

    first test program works. fix addressing and pll next

--- instruction_rom.v
+++ instruction_rom.v
@@ -2,14 +2,14 @@ module instruction_rom(input [4:0] addr, output reg [31:0] instr);
 
 always @* begin
 	case (addr)
-	5'd0: instr = 32'h01000093; // addi x1 x0 16 32'h0000_0093; // addi x1 x0 0
-	5'd1: instr = 32'h00102023; // sw x1, 0(x0) 32'h0100_0113; // addi x2 x0 16
-	5'd2: instr = 32'h00002183; // lw x3 0(x0) 32'h0640_0193; // addi x3 x0 100
-	5'd3: instr = 32'h06418193; // addi x3 x3 100 32'h0080_0213; // addi x4 x0 8
-	5'd4: instr = 32'h003020a3; // sw x3 1(x0)  32'h0020_82b3; // add  x5 x1 x2
-	5'd5: instr = 32'h00102203; // lw x4 1(x0) 32'h0041_8333; // add  x6 x3 x4
-	//5'd6: instr = 32'h0050_a023; // sw x5 0(x1)
-	//5'd7: instr = 32'h0061_2223; // sw x6 4(x2)
+	5'd0: instr = 32'h0000_0093; // addi x1 x0 0
+	5'd1: instr = 32'h0100_0113; // addi x2 x0 16
+	5'd2: instr = 32'h0640_0193; // addi x3 x0 100
+	5'd3: instr = 32'h0080_0213; // addi x4 x0 8
+	5'd4: instr = 32'h0020_82b3; // add  x5 x1 x2
+	5'd5: instr = 32'h0041_8333; // add  x6 x3 x4
+	5'd6: instr = 32'h0050_a023; // sw x5 0(x1)
+	5'd7: instr = 32'h0061_2223; // sw x6 4(x2)
 	default: instr = 32'hffff_ffff; // halt
 	endcase
 end
--- processor.v
+++ processor.v
@@ -1,29 +1,26 @@
-module processor(input CLOCK_50,
-					  output reg [4:0] pc,
-					  output [31:0] instr,
-					  output reg [4:0] rd1,
-					  output reg [4:0] rd2,
-					  output [31:0] rr1,
-					  output [31:0] rr2,
-					  output reg [4:0] w,
-					  output [31:0] alu_res,
-					  output [31:0] wd,
-					  output reg_wrenable,
-					  output mem_wrenable,
-					  output reg [31:0] alu_src,
-					  output [31:0] mem_res);
-// TODO: add instruction ROM, reg file, data RAM, control unit
+module processor(input CLOCK_50);
+//					  output reg [4:0] pc,
+//					  output [31:0] instr,
+//					  output reg [4:0] rd1,
+//					  output reg [4:0] rd2,
+//					  output [31:0] rr1,
+//					  output [31:0] rr2,
+//					  output reg [4:0] w,
+//					  output [31:0] alu_res,
+//					  output [31:0] wd,
+//					  output reg_wrenable,
+//					  output mem_wrenable,
+//					  output reg [31:0] alu_src,
+//					  output [31:0] mem_res);
 
-//reg [4:0] pc;
-//reg [4:0] rd1;
-//reg [4:0] rd2;
-//reg [4:0] w;
 
-//wire [31:0] instr;
-//wire [31:0] rr1;
-//wire [31:0] rr2;
-//wire [31:0] alu_res;
-//wire [31:0] wd;
+// Note: this supports add, addi, sw, and lw.
+// The first test program runs on this. Other stuff is in the works...
+
+reg [4:0] pc;
+reg [4:0] rd1;
+reg [4:0] rd2;
+reg [4:0] w;
 
 initial begin
 	pc = 0;
@@ -32,10 +29,22 @@ initial begin
 	w = 0;
 end
 
-//wire reg_wrenable;
-//wire mem_wrenable;
-//wire [31:0] alu_src;
-//wire [31:0] mem_res;
+wire [31:0] instr;
+
+wire [31:0] rr1;
+wire [31:0] rr2;
+wire [31:0] alu_res;
+wire [31:0] wd;
+wire reg_wrenable;
+wire mem_wrenable;
+reg [31:0] alu_src;
+wire [31:0] mem_res;
+
+wire clk;
+pll pll(CLOCK_50, clk);
+
+wire mem_clk;
+assign mem_clk = ~clk;
 
 // connect to reg file
 regfile rf(.clk(CLOCK_50), .read_reg1(rd1), .read_reg2(rd2),
@@ -47,14 +56,10 @@ instruction_rom rom(pc, instr);
 
 // instantiate data memory
 // write enabled only on stores
-
-wire mem_clk;
-assign mem_clk = ~CLOCK_50;
-
-fakeram ram(.address(alu_res[4:0]), .clock(mem_clk), .data(rr2), .wren(mem_wrenable), .q(mem_res));
+ram ram(.address(alu_res[4:0]), .clock(mem_clk), .data(rr2), .wren(mem_wrenable), .q(mem_res));
 
 always @(posedge CLOCK_50) begin
-	if (pc < 5'd5) pc <= pc + 1'b1;
+	if (pc < 5'd7) pc <= pc + 1'b1;
 	else pc <= pc;
 end
 
