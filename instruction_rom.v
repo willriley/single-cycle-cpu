d91b2117a1622ef4125c165979ef5265493bff83
willriley <wriley.email@gmail.com>
2020-03-18 16:18:10 -0400

    beq and bne working in sim

--- instruction_rom.v
+++ instruction_rom.v
@@ -2,25 +2,13 @@ module instruction_rom(input [4:0] addr, output reg [31:0] instr);
 
 always @* begin
 	case (addr)
-	5'd0: instr = 32'h00800293;
-	5'd1: instr = 32'h00f00313;
-	5'd2: instr = 32'h0062a023;
-	5'd3: instr = 32'h005303b3;
-	5'd4: instr = 32'h40530e33;
-	5'd5: instr = 32'h03c384b3;
-	5'd6: instr = 32'h00428293;
-	5'd7: instr = 32'hffc2a903;
-	5'd8: instr = 32'h41248933;
-	5'd9: instr = 32'h00291913;
-	5'd10: instr = 32'h0122a023;
-//	5'd0: instr = 32'h0000_0093; // addi x1 x0 0
-//	5'd1: instr = 32'h0100_0113; // addi x2 x0 16
-//	5'd2: instr = 32'h0640_0193; // addi x3 x0 100
-//	5'd3: instr = 32'h0080_0213; // addi x4 x0 8
-//	5'd4: instr = 32'h0020_82b3; // add  x5 x1 x2
-//	5'd5: instr = 32'h0041_8333; // add  x6 x3 x4
-//	5'd6: instr = 32'h0050_a023; // sw x5 0(x1)
-//	5'd7: instr = 32'h0061_2223; // sw x6 4(x2)
+	5'd0: instr = 32'h00f00093;  // addi x1, x0, 15
+	5'd1: instr = 32'h00f00113;  // addi x2, x0, 15
+	5'd2: instr = 32'h00208663;  // beq x1, x2, 12
+	5'd3: instr = 32'h00202023;  // sw x2 0(x0)
+	5'd4: instr = 32'hffff_ffff; // halt
+	5'd5: instr = 32'h04500093;  // addi x1, x0, 69
+	5'd6: instr = 32'h00102023;  // sw x1, 0(x0)
 	default: instr = 32'hffff_ffff; // halt
 	endcase
 end
--- processor.v
+++ processor.v
@@ -1,67 +1,94 @@
-module processor(input CLOCK_50);
-//					  output reg [4:0] pc,
-//					  output [31:0] instr,
-//					  output [4:0] rr1,
-//					  output [4:0] rr2,
-//					  output [31:0] rd1,
-//					  output [31:0] rd2,
-//					  output [4:0] w,
-//					  output [31:0] alu_res,
-//					  output [31:0] wd,
-//					  output reg_wrenable,
-//					  output mem_wrenable,
-//					  output reg [31:0] alu_src,
-//					  output reg [4:0] alu_op,
-//					  output [7:0] mem_addr,
-//					  output [31:0] mem_res,
-//					  output halt);
+module processor(input CLOCK_50,
+					  output reg [4:0] pc,
+					  output [31:0] instr,
+					  output [4:0] rr1,
+					  output [4:0] rr2,
+					  output [31:0] rd1,
+					  output [31:0] rd2,
+					  output [4:0] w,
+					  output [31:0] alu_res,
+					  output reg [31:0] wd,
+					  output reg reg_wrenable,
+					  output reg mem_wrenable,
+					  output reg [31:0] alu_src,
+					  output reg [4:0] alu_op,
+					  output [7:0] mem_addr,
+					  output [31:0] mem_res,
+					  output reg halt);
 
+// TODO: add instruction ROM; create control module; increase clock speed
 
-reg [4:0] pc; // program counter
-wire [31:0] instr; // current instruction
-reg halt;
+parameter HALT = 7'b1111111;
+parameter LOAD = 7'b0000011;
+parameter STORE = 7'b0100011;
+parameter ITYPE = 7'b0010011;
+parameter BTYPE = 7'b1100011;
+parameter RTYPE = 7'b0010011;
+
+//reg [4:0] pc; // program counter
+//wire [31:0] instr; // current instruction
+//reg halt;
 
 initial begin
 	pc = 0;
 	halt = 0;
 end
 
-always @(posedge clock) begin
-	pc <= halt ? pc : pc + 1'b1;
+always @(posedge CLOCK_50) begin
+	if (halt) pc <= pc;
+	else begin
+		// increment pc by 1 if instr isn't a branch
+		// and the condition isn't met
+		if (instr[6:0] != BTYPE || !instr[12] && alu_res || instr[12] && !alu_res) begin
+			pc <= pc + 1'b1;
+		end
+		else pc <= pc + imm;
+	end
 end
 
 // register file ports
-wire [4:0] rr1; // read reg 1
-wire [4:0] rr2; // read reg 2
-reg reg_wrenable; // write enable
-wire [4:0] w; // write reg
-reg [31:0] wd; // write data
-wire [31:0] rd1; // read data 1
-wire [31:0] rd2; // read data 2
-
-// alu ports
-reg [4:0] alu_op;
-reg [31:0] alu_src;
-wire [31:0] alu_res;
-
-// data memory ports
-reg mem_wrenable;
-wire [31:0] mem_res;
-
-wire clock;
-pll pll(CLOCK_50, clock);
+//wire [4:0] rr1; // read reg 1
+//wire [4:0] rr2; // read reg 2
+//reg reg_wrenable; // write enable
+//wire [4:0] w; // write reg
+//reg [31:0] wd; // write data
+//wire [31:0] rd1; // read data 1
+//wire [31:0] rd2; // read data 2
+//
+//// alu ports
+//reg [4:0] alu_op;
+//reg [31:0] alu_src;
+//wire [31:0] alu_res;
+//
+//// data memory ports
+//reg mem_wrenable;
+//wire [31:0] mem_res;
+//
+//wire clock;
+//pll pll(CLOCK_50, clock);
 
 // 180 phase shift for memory clock
 wire mem_clk;
-assign mem_clk = ~clock;
+assign mem_clk = ~CLOCK_50;
 
 // connect to reg file
-regfile rf(.clk(clock), .read_reg1(rr1), .read_reg2(rr2),
+regfile rf(.clk(CLOCK_50), .read_reg1(rr1), .read_reg2(rr2),
    		  .write_reg(w), .write_data(wd), .write_enable(reg_wrenable),
 			  .read_data1(rd1), .read_data2(rd2));
 				
 // instantiate instruction memory
 instruction_rom rom(pc, instr);
+
+// immediate generator
+reg [31:0] imm; 
+always @* begin
+	case (instr[6:0])
+	STORE: imm = {instr[31:25], instr[11:7]};
+	LOAD, ITYPE: imm = instr[31:20];
+	BTYPE: imm = {instr[31], instr[7], instr[30:25], instr[11:9]};
+	default: imm = 0;
+	endcase	
+end
 
 // control unit: set flags based on opcode
 always @* begin
@@ -73,20 +100,23 @@ always @* begin
 	wd = alu_res;
 	
 	case (instr[6:0])
-	7'b1111111: halt = 1'b1;
-	7'b0100011: begin // s-type
-		alu_src = {instr[31:25], instr[11:7]};
+	HALT: halt = 1'b1;
+	STORE: begin // s-type
+		alu_src = imm;
 		mem_wrenable = 1'b1;
 	end
-	7'b0010011: begin // i-type
+	ITYPE: begin // i-type
 		alu_op = {2'd0, instr[14:12]};
-		alu_src = instr[31:20];
+		alu_src = imm;
 		reg_wrenable = 1'b1;
 	end
-	7'b0000011: begin // load
-		alu_src = instr[31:20];
+	LOAD: begin // load
+		alu_src = imm;
 		reg_wrenable = 1'b1;
 		wd = mem_res;
+	end
+	BTYPE: begin // b-type
+		alu_op = 5'b10000; // subtraction
 	end
 	default: begin // r-type
 		alu_op = {instr[30], instr[25], instr[14:12]};
@@ -95,11 +125,12 @@ always @* begin
 	endcase
 end
 
+
 // instantiate alu
 alu alu(.opc(alu_op), .op1(rd1),
 		  .op2(alu_src), .res(alu_res));
 
-wire [7:0] mem_addr;
+// wire [7:0] mem_addr;
 assign mem_addr = alu_res[9:2];
 
 // instantiate data memory
